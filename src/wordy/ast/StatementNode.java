0668378abf5329b4bb96caedc05d751bead5fe7b
Paul Cantrell <cantrell@pobox.com>
2019-09-09 07:41:07 -0500

    run() and evaluate() can now be fully abstract

--- src/wordy/ast/StatementNode.java
+++ src/wordy/ast/StatementNode.java
@@ -3,7 +3,5 @@ package wordy.ast;
 import wordy.interpreter.EvaluationContext;
 
 public abstract class StatementNode extends ASTNode {
-    public void run(EvaluationContext context) {
-        throw new UnsupportedOperationException("not implemented yet for " + getClass());
-    }
+    public abstract void run(EvaluationContext context);
 }
